51d0dc8f45e41209825f64cb45501265dc12bc5d
Oloxx <alexrodart03@gmail.com>
2023-05-12 19:07:16 +0200

    Add Cicle to edit profile form

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,6 +18,7 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['string', 'max:255'],
             'cognoms' => ['string', 'max:255'],
+            'cicle_id' => ['integer'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
